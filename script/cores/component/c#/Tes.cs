29ec98a639d79c7fb8f58669db41a4a7c5f2a387
Phoenix-162 <misivoya32@gmail.com>
2025-12-25 14:41:48 +0800

    a

--- script/cores/component/c#/Tes.cs
+++ script/cores/component/c#/Tes.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 [GlobalClass]
-public partial class Plugin : Node
+public  partial class Plugin : Node
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
